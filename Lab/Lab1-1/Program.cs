3103776bd100a36aa372a674d01bb2a3ed58b0ea
oilchill <nummung.oil_umami@hotmail.com>
2015-08-21 16:59:22 +0700

    Without flag

--- Lab/Lab1-1/Program.cs
+++ Lab/Lab1-1/Program.cs
@@ -16,9 +16,9 @@ namespace Lab1_1
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
 
             // process
-            while (flag)
-            {
-                flag = false;
+            //while (flag)
+           // {
+              //  flag = false;
                 for (int i = 0; i != input.Length - 1; i++)
                 {
                     if (input[i] < input[i + 1])
@@ -26,10 +26,10 @@ namespace Lab1_1
                         int temp = input[i];
                         input[i] = input[i + 1];
                         input[i + 1] = temp;
-                        flag = true;
+                //        flag = true;
                     }
                 }
-            }
+            //}
 
             // output
             for (int i = 0; i != input.Length; i++)
